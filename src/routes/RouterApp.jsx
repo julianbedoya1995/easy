b574dea5a7e9cd987c8028fe3755c8c1c92a9248
luis espejo <hola@luisespejo.com>
2024-06-16 18:18:17 -0500

    Cambio de nombre del archivo de Router a RouterApp para evitar conflicto

---
+++ src/routes/RouterApp.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Feed } from "../components/pages/Feed";
+import { Categories } from "../components/pages/Categories";
+import { Error } from "../components/pages/Error";
+import { Profile } from "../components/pages/Profile";
+import { Recipes } from "../components/pages/Recipes";
+
+export const RouterApp = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/categorias" element={<Categories />} />
+        <Route path="/recetas" element={<Recipes />} />
+        <Route path="/mi-perfil" element={<Profile />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
