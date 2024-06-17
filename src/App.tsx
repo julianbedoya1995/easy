20a21ddd6db292c213c14c5eeda541b005dc8d54
luis espejo <hola@luisespejo.com>
2024-06-16 22:14:45 -0500

    Condicional para no renderizar el menú si es una ruta inexistente

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import { RoutesMain } from "./routes/RoutesMain";
+import { Menu } from "./components/layout/menu/Menu";
+import { useLocation } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <RoutesMain />
-    </>
+  const location = useLocation();
+  const routes = RoutesMain().props.children.props.children;
+
+  // 1. El método some() itera sobre el objeto routes y devuelve true si al menos uno de los elementos del objeto cumple con la condición especificada en la función callback. Si ningún elemento cumple con la condición, devuelve false.
+
+  // 2. La anotación de tipo (route: { props: { path: string } }) indica que el parámetro route es un objeto que tiene una propiedad props, que a su vez tiene una propiedad path de tipo string.
+  const isValidRoute = routes.some(
+    (route: { props: { path: string } }) =>
+      route.props.path === location.pathname
   );
+
+  if (isValidRoute) {
+    return (
+      <>
+        <Menu />
+      </>
+    );
+  } else {
+    return <RoutesMain />;
+  }
 }
 
 export default App;
