6eb5befb9d003cb0d5b9f3ea65b97d5edec85239
luis espejo <hola@luisespejo.com>
2024-06-16 20:58:17 -0500

    Importación de las rutas actualizadas

--- src/routes/RoutesMain.tsx
+++ src/routes/RoutesMain.tsx
@@ -1,20 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Feed } from "../components/pages/Feed";
-import { Categories } from "../components/pages/Categories";
-import { Error } from "../components/pages/Error";
-import { Profile } from "../components/pages/Profile";
-import { Recipes } from "../components/pages/Recipes";
-import { MenuComponent } from "../components/Layout/MenuComponent/MenuComponent";
+import { Menu } from "../components/Layout/menu/Menu";
+import { Feed } from "../components/pages/feed/Feed";
+import { Calendar } from "../components/pages/calendar/Calendar";
+import { Profile } from "../components/pages/profile/Profile";
+import { Error } from "../components/pages/error/Error";
 
 export const RoutesMain = () => {
   return (
     <BrowserRouter>
-      <MenuComponent />
+      <Menu />
       <Routes>
         <Route path="/" element={<Feed />} />
         <Route path="/feed" element={<Feed />} />
-        <Route path="/categorias" element={<Categories />} />
-        <Route path="/recetas" element={<Recipes />} />
+        <Route path="/calendario" element={<Calendar />} />
         <Route path="/mi-perfil" element={<Profile />} />
         <Route path="*" element={<Error />} />
       </Routes>
