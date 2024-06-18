59eaf10c9b9b38211c088d8052fed13e9200ba38
luis espejo <hola@luisespejo.com>
2024-06-17 22:03:42 -0500

    Estructura para el feed

--- src/components/pages/feed/Feed.tsx
+++ src/components/pages/feed/Feed.tsx
@@ -1,9 +1,21 @@
-import { ShowRecipes } from "../../../helpers/ShowRecipes"
+import { NavLink } from "react-router-dom";
+import { ListRecipes } from "../../../data/ListRecipes";
+import "./Feed.css";
 
 export const Feed = () => {
+  const showList = ListRecipes();
+
   return (
-    <>
-      <ShowRecipes />
-    </>
+    <section>
+      {showList.map(({ title, photo }, index) => (
+        <article key={index}>
+          <img src={photo.url} alt={photo.alt} />
+          <div>
+            <h2>{title}</h2>
+            <NavLink to="#">Ver mas...</NavLink>
+          </div>
+        </article>
+      ))}
+    </section>
   );
 };
