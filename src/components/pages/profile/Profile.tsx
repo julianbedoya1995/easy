4738f506cc86801bef66811024703a29adb92fb4
julian <julianjaramillo6895@gmail.com>
2024-06-21 22:40:54 -0500

    importación del componente configuration

--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -1,4 +1,10 @@
+import { Configuration } from "../configuration/Configuration";
 import "./Profile.css";
+
 export const Profile = () => {
-  return <></>;
+  return (
+    <>
+      <Configuration />
+    </>
+  );
 };
